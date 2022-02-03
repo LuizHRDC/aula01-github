f223e8e670637385cef669078e34f6f256bf93df
LuizHRDC <luizhrdc137@gmail.com>
2022-02-03 01:54:46 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//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
+	}
+
+}
