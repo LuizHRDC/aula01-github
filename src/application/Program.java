463ce5e9792fd2f077d9b2b1788ef5faf5127dd8
LuizHRDC <luizhrdc137@gmail.com>
2022-02-03 18:34:51 -0300

    Mensagem explicativa

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,8 +3,7 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//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
+		System.out.println("Hello, goodbye!");
 	}
 
 }
